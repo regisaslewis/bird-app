e0d6bc6f30cdc36d834fa9854bd5be7cac378ffb
RegisLewis <regisaslewis@gmail.com>
2023-11-21 13:36:01 -0800

    initial commit

---
+++ migrations/versions/4d49c064aa11_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4d49c064aa11
+Revises: 
+Create Date: 2023-11-21 13:28:54.241410
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4d49c064aa11'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
